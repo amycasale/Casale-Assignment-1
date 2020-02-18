ba35564a4f1fa3ff1e43175b84a8f1c22b9022b0
Amy Casale <a.casale@setonhill.edu>
2020-02-18 12:02:33 -0500

    Ready for review

---
+++ Casale Assignment #1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Casale Assignment #1
+//
+//  Created by Amy Casale on 2/3/20.
+//  Copyright © 2020 Amy Casale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
